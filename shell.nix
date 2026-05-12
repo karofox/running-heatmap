87b0f32b940b76db9b66633d85bca708e4de7945
Karolina Kozubik <k4aro0@gmail.com>
2026-05-12 16:53:12 +0200

    feat: add nix direnv

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    python311
+    python311Packages.pip
+    python311Packages.virtualenv
+    
+    stdenv.cc.cc.lib
+    zlib
+    glib
+  ];
+
+  shellHook = ''
+    # Allow pip-installed binaries to find dynamically linked C libraries
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      pkgs.glib
+    ]}:$LD_LIBRARY_PATH
+    
+    # Ensures pip installs into the direnv-created virtual environment
+    export PIP_PREFIX=$(pwd)/.direnv/python-venv
+  '';
+}
